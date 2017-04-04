24dcfb1a2bb53ab7b70fd7f3cfb41a6ba0f21a9c
michalczyzowicz <michalczyzowicz@gmail.com>
2017-04-04 15:11:26 +0200

    MyOnet First commit

---
+++ src/main/java/Moje/MyOnetSearch.java
@@ -0,0 +1,33 @@
+package Moje;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MyOnetSearch {
+
+	@FindBy (id = "searchQuery")
+	private WebElement searchText;
+	@FindBy (id = "searchSubmit")
+	private WebElement button;
+	@FindBy (css = ".boxResult2 > .box")
+	private List<WebElement> results;
+	
+	public void setSearchText (String text) {
+		this.searchText.sendKeys(text);
+	}
+	
+	public void click() {
+		button.click();
+	}
+	
+	public List<WebElement> getTitle() {
+		return results;
+	}
+	
+	public List<WebElement> getResults() {
+		return results;
+	}
+	
+}
