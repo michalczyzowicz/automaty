085435782d9032ee213e984287a65e36072f4885
michalczyzowicz <michalczyzowicz@gmail.com>
2017-04-05 15:09:40 +0200

    Praca z różnymi metodami

--- src/main/java/Moje/Próbne.java
+++ src/main/java/Moje/Próbne.java
@@ -15,12 +15,11 @@ public class Próbne {
 		Thread.sleep(3000);
 		driver.getTitle();
 		System.out.println(driver.getTitle());
-		
-		/*Thread.sleep(1000);
+		Thread.sleep(1000);
 		driver.findElement(By.cssSelector(".close-modal")).click();
 		Thread.sleep(1000);
 		Actions action = new Actions(driver);
-		action.clickAndHold(driver.findElement(By.id("topNav3"))).build().perform();*/
+		action.clickAndHold(driver.findElement(By.id("topNav3"))).build().perform();
 		//Select select = new Select(driver.findElement(By.id("subMenu3")));
 		//select.selectByVisibleText("For Her");
 		//driver.findElement(By.id("topNav3")).click();
