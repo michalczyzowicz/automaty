18b0288a75924c78415a01df4890f3e54ad82904
michalczyzowicz <michalczyzowicz@gmail.com>
2017-04-05 15:49:05 +0200

    Poprawienie próbnych

--- src/main/java/Moje/Próbne.java
+++ src/main/java/Moje/Próbne.java
@@ -11,16 +11,13 @@ public class Próbne {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new FirefoxDriver();
-		driver.get("http://szukaj.onet.pl/wyniki.html?qt=nikon/");
+		driver.get("http://barnesandnoble.com/");
 		Thread.sleep(3000);
 		driver.getTitle();
 		System.out.println(driver.getTitle());
-		
-		/*Thread.sleep(1000);
-		driver.findElement(By.cssSelector(".close-modal")).click();
 		Thread.sleep(1000);
 		Actions action = new Actions(driver);
-		action.clickAndHold(driver.findElement(By.id("topNav3"))).build().perform();*/
+		action.click(driver.findElement(By.id("topNav3"))).build().perform();
 		//Select select = new Select(driver.findElement(By.id("subMenu3")));
 		//select.selectByVisibleText("For Her");
 		//driver.findElement(By.id("topNav3")).click();
