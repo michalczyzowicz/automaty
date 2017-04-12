97eb87c7e73e00327f8f11e863eaa418a6485383
michalczyzowicz <michalczyzowicz@gmail.com>
2017-04-12 18:43:09 +0200

    Kolejny

--- src/test/java/Zajecia/BookingSearchTest.java
+++ src/test/java/Zajecia/BookingSearchTest.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BookingSearchTest {
+import Moje.BookingSearch;
 
+public class BookingSearchTest {
+	private static WebDriver driver;
 	
 	@Test
 	public void search() {
