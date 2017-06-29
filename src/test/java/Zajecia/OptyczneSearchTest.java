e00cd2c02eda6714177a2e5b2f02dba6ec8e271b
michalczyzowicz <michalczyzowicz@gmail.com>
2017-06-29 12:28:40 +0200

    Commit

--- src/test/java/Zajecia/OptyczneSearchTest.java
+++ src/test/java/Zajecia/OptyczneSearchTest.java
@@ -1,19 +1,18 @@
 package Zajecia;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
-
-import Zajecia.OptyczneSearch;
-
-import static org.junit.Assert.assertEquals;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class OptyczneSearchTest{
-
+	private static final Logger LOG = LoggerFactory.getLogger(OptyczneSearchTest.class);
     public static WebDriver driver;
 
     @BeforeClass
@@ -29,7 +28,8 @@ public class OptyczneSearchTest{
 
     @Test
     public void testSearch() {
-        OptyczneSearch optyczneSearch = PageFactory.initElements(driver,  OptyczneSearch.class);
+        LOG.info("test");
+    	OptyczneSearch optyczneSearch = PageFactory.initElements(driver,  OptyczneSearch.class);
         optyczneSearch.setSearchText("nikon");
         optyczneSearch.click();
         assertEquals(optyczneSearch.getResults().size(), 20);
